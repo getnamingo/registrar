dffef3b71a1c1bfb4adf12a38f1d808dc30da8e4
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-11 10:39:47 +0200

    Update db.php

--- wdrp/db.php
+++ wdrp/db.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Indera Registrar System
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ *
+ * @license MIT
+ */
+
 try {
     $pdo = new PDO("mysql:host={$config['db']['host']};dbname={$config['db']['dbname']}", $config['db']['username'], $config['db']['password']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
