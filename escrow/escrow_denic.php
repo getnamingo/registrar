8af9b80e6002cd03ebb4c678e918b1369d8a113a
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-11 10:42:30 +0200

    Create

---
+++ escrow/escrow_denic.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Kaya Registrar Escrow library
+ *
+ * Written in 2023 by Taras Kondratyuk (https://getpinga.com)
+ *
+ * @license MIT
+ */
+
+// Include the Composer autoloader
+require_once 'vendor/autoload.php';
+
+// Use the DENICEscrowGenerator class
+use Pinga\Kaya\DENICEscrowGenerator;
+
+$full = '/opt/kaya/full.csv';
+$hdl = '/opt/kaya/hdl.csv';
+$pdo = new PDO("mysql:host=localhost;dbname=database", 'username', 'password');
+
+// Create the Escrow generator
+$escrowGenerator = new DENICEscrowGenerator($pdo, $full, $hdl);
+
+// Generate the escrow deposits
+$escrowGenerator->generateFull();
+$escrowGenerator->generateHDL();
+
+// Submit the escrow deposits
+//exec('./escrow-rde-client -c config.yaml');
