b37d2ef6dba3f45461a2816f4a6ce4c974099db3
Namingo <help@namingo.org>
2026-06-10 15:17:12 +0300

    v1.1.7 release preparation

--- docs/install.sh
+++ docs/install.sh
@@ -140,7 +140,7 @@ install_rdap_and_whois_services() {
     echo "Installing RDAP & WHOIS services..."
 
     # Clone the registrar repository
-    git clone --branch v1.1.6 --single-branch https://github.com/getnamingo/registrar /opt/registrar
+    git clone --branch v1.1.7 --single-branch https://github.com/getnamingo/registrar /opt/registrar
 
     # Setup for WHOIS service
     cd /opt/registrar/whois
@@ -242,7 +242,7 @@ install_php_repo() {
   fi
 }
 
-echo "==== Namingo Registrar v1.1.6 ===="
+echo "==== Namingo Registrar v1.1.7 ===="
 echo
 echo "This tool will guide you through installing Namingo Registrar with your preferred billing system."
 echo
