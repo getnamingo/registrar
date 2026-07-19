63061b89fb15ffd12268f1eb239632ea2eb9dc41
Namingo <help@namingo.org>
2026-07-19 18:08:01 +0300

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -649,8 +649,11 @@ rm -f "$tmp_cron" /tmp/crontab.err
 mariadb -u $db_user -p$db_pass registrar < /var/www/install/sql/structure.sql
 mariadb -u $db_user -p$db_pass registrar < /var/www/install/sql/content.sql
 
+wget https://raw.githubusercontent.com/getnamingo/registrar/refs/heads/main/docs/bin/configure-client-fields.php -O /tmp/configure-client-fields.php
+
 echo "Configuring required client fields."
 php /tmp/configure-client-fields.php
+rm -f /tmp/configure-client-fields.php
 
 echo ""
 echo "=================================================="
