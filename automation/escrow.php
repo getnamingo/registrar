7e80479cd04336578dbd8907c47f879f03e649f5
Namingo <help@namingo.org>
2026-09-02 01:18:03 +0300

    Replaced error_log with logger

--- automation/escrow.php
+++ automation/escrow.php
@@ -26,8 +26,8 @@ try {
     $pdo = new PDO("mysql:host={$config['db']['host']};dbname={$config['db']['dbname']}", $config['db']['username'], $config['db']['password']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    error_log('Database connection error: ' . $e->getMessage());
-    exit('Oops! Something went wrong.');
+    $log->error('Database connection error: ' . $e->getMessage());
+    exit(1);
 }
 
 // Determine full class name with namespace
