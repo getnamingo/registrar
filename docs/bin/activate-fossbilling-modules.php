13ef48eb84b24e53d1215b6bc839cc3cc20cd004
Namingo <help@namingo.org>
2026-09-24 23:41:17 +0300

    Added script to activate modules for FOSSBilling

---
+++ docs/bin/activate-fossbilling-modules.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+require '/var/www/load.php';
+
+$modules = [
+    'servicedns',
+    'registrar',
+    'domaincontactvalidation',
+    'validation',
+    'contact',
+    'tmch',
+    'whois',
+];
+
+try {
+    $di['is_cron'] = true;
+    $extensions = $di['mod_service']('extension');
+
+    printf("Activating modules:");
+
+    foreach ($modules as $module) {
+        if ($extensions->isExtensionActive('mod', $module)) {
+            printf(" %s[active]", $module);
+            continue;
+        }
+
+        $extensions->activateExistingExtension([
+            'type' => 'mod',
+            'id'   => $module,
+        ]);
+
+        printf(" %s[ok]", $module);
+    }
+
+    echo PHP_EOL;
+
+} catch (Throwable $e) {
+    fwrite(STDERR, PHP_EOL . "Module activation failed: " . $e->getMessage() . PHP_EOL);
+    exit(1);
+}
