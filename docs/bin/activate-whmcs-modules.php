dec895316c2f9d8f1c1b490686fdb80fe87a9b94
Namingo <help@namingo.org>
2026-09-24 23:43:07 +0300

    Create activate-whmcs-modules.php

---
+++ docs/bin/activate-whmcs-modules.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+require '/var/www/whmcs/init.php';
+
+$modules = [
+    'whmcs_dns',
+    'namingo_registrar',
+    'namingo_contact_validation',
+];
+
+try {
+    printf("Activating modules:");
+
+    foreach ($modules as $module) {
+        $result = localAPI('ActivateModule', [
+            'moduleType' => 'addon',
+            'moduleName' => $module,
+        ]);
+
+        if (($result['result'] ?? '') !== 'success') {
+            throw new RuntimeException(
+                $module . ': ' . ($result['message'] ?? 'activation failed')
+            );
+        }
+
+        printf(" %s[ok]", $module);
+    }
+
+    echo PHP_EOL;
+
+} catch (Throwable $e) {
+    fwrite(STDERR, PHP_EOL . "Module activation failed: " . $e->getMessage() . PHP_EOL);
+    exit(1);
+}
